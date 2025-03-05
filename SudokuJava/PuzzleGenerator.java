ebce3dcc269bf84a9f1852cb59873ea20f24f0f1
mg218 <mgoss212@gmail.com>
2025-03-05 01:27:21 -0600

    Can now create puzzles ready to be solved.

--- SudokuJava/PuzzleGenerator.java
+++ SudokuJava/PuzzleGenerator.java
@@ -66,4 +66,52 @@ public class PuzzleGenerator {
         
         return empty;
     }
+    //Works only with 9x9 default grids at the moment
+    public int[][] finishedGrid(int diffficulty,boolean useExisting, int[][] existingGrid){
+        int[][] fullPuzzle;
+        if(useExisting==true){
+            fullPuzzle=existingGrid;
+        }else{
+            fullPuzzle=gridOut();
+        }
+        int numRemove=0;//Variable that decides which difficulty to use
+        switch (diffficulty) {
+            case 0://easy difficulty
+                numRemove=(int)(Math.random()*30)+25;
+                break;
+            case 1://medium
+                numRemove=(int)(Math.random()*45)+35;
+                break;
+            case 2://hard
+                numRemove=(int)(Math.random()*55)+48;
+                break;
+            case 3://very hard
+                numRemove=(int)(Math.random()*69)+57;
+                break;
+            default:
+                System.err.println("Valid Difficult not entered");
+                break;
+        }
+        int yesno=0;
+        while(true){//keep 
+            gridLoop:
+            for(int i=0;i<size;i++){
+                for(int j=0;j<size;j++){
+                    yesno=(int)(Math.random()*2)+1;
+                    if(yesno==2){
+                        fullPuzzle[i][j]=0;
+                        numRemove--;
+                    }
+                    if(numRemove<=0)
+                        break gridLoop;
+                }
+            }
+            if(numRemove<=0)
+                break;
+
+        }
+
+
+        return fullPuzzle;
+    }
 }
